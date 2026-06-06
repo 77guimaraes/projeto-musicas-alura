e0c18f950d517f3e52a1b517c4351e2772bed84e
Irã Guimarães <iranmcg016@gmail.com>
2026-06-05 23:46:30 -0300

    Comenta os métodos para melhor entendimento

--- src/br/com/alura/minhasMusicas/modelos/audio.java
+++ src/br/com/alura/minhasMusicas/modelos/audio.java
@@ -6,21 +6,24 @@ public class audio {
     private int totalCurtidas;
     private double classificacao;
 
-
+    //Permite visualizar titulo
     public String getTitulo() {
         return titulo;
     }
 
+    //Permite alterar titulo
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
+    //Metodo para Curtir
     public void Curte(){
         this.totalCurtidas++;
     }
 
+    //Metodo para reproduzir
     public void reproduz(){
         this.totalReproducoes++;
-
+    //quando chamar esse métodos, adiciona mais um no total de repoduções
     }
 }
