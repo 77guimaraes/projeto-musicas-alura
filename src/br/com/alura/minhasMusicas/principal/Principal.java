8164375b09a2731312d197fdd009c3c2e90fadfc
Irã Guimarães <iranmcg016@gmail.com>
2026-06-06 00:08:32 -0300

    Cria class Principal com dois objetos com base em Musica e Podcast, cada um contendo seus laços de repetição

--- src/br/com/alura/minhasMusicas/principal/Principal.java
+++ src/br/com/alura/minhasMusicas/principal/Principal.java
@@ -1,4 +1,31 @@
 package br.com.alura.minhasMusicas.principal;
 
+import br.com.alura.minhasMusicas.modelos.Musica;
+import br.com.alura.minhasMusicas.modelos.Podcast;
+
 public class Principal {
+    public static void main(String[] args) {
+
+        //Objeto minhaMusica
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Time");
+        minhaMusica.setCantor("Pink Floyd");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for  (int i = 0; i < 50; i++) {
+            minhaMusica.Curte();
+        }
+
+        //Objeto meuPodcast
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("PodPah");
+        meuPodcast.setApresentador("Mítico");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+    }
 }
